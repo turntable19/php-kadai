15d5477d92fe3c094e871c4492b70f9ce108ea7a
turntable19 <minatomina811@gmail.com>
2023-01-15 22:07:39 +0900

    initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+$a = 3;
+$b = 7;
+echo $a + $b;
+$array_month = [
+    "January" => "1月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "July" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月"
+];
+echo $array_month ["August"];
+$hello = "Hello";
+$name = "Minato";
+$world = "`s World";
+echo $hellos = $hello.$name.$world;
+
+$calendar_2018= [
+    "January" => "1月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "July" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月"
+];
+  
+  // 12月を表示する
+  echo $calendar_2018["December"];
+?>
