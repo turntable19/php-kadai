1c1bc90ebdddcab762ce47e2a1a8cfb0228e7af9
turntable19 <minatomina811@gmail.com>
2023-02-26 21:58:25 +0900

    saiteisyutu

--- index3.php
+++ index3.php
@@ -12,18 +12,9 @@ function add($a,$b){
 echo add(2,5);
 
 function product($arr){
-    $result = 0;
+    $result = 1;
     for ($i = 0; $i < count($arr); $i++) {
-        if($i == 0){
-            $result += $arr[$i];
-        }else{
-            $result *= $arr[$i];
-        }
-    }
-    return $result;
-    $result = $arr[0]; 
-    for ($i = 1; $i < count($arr); $i++) {
-            $result *= $arr[$i];
+        $result *= $arr[$i];
     }
     return $result;
 }
@@ -39,25 +30,25 @@ function max_array($arr){
      }
     
      return $max_number;
-     }
+}
      
-     echo max_array(array(6, 2, 3, 10, 5));
+echo max_array(array(6, 2, 3, 10, 5));
 
-     $text = '<p>Test paragraph.</p><!-- Comment --> <a href="#fragment">Other text</a>';
-     echo strip_tags($text);
-     echo "\n";
-     echo strip_tags($text, '<p><a>');
+$text = '<p>Test paragraph.</p><!-- Comment --> <a href="#fragment">Other text</a>';
+echo strip_tags($text);
+echo "\n";
+echo strip_tags($text, '<p><a>');
      
 $stack = array("orange", "banana");
- array_push($stack, "apple", "raspberry");
- print_r($stack);
+array_push($stack, "apple", "raspberry");
+print_r($stack);
 
- $array1 = array("color" => "red", 2, 4);
+$array1 = array("color" => "red", 2, 4);
 $array2 = array("a", "b", "color" => "green", "shape" => "trapezoid", 4);
- $result = array_merge($array1, $array2);
- print_r($result);
+$result = array_merge($array1, $array2);
+print_r($result);
 
- date_default_timezone_set('UTC');
+date_default_timezone_set('UTC');
 echo "July 1, 2000 is on a " . date("l", mktime(0, 0, 0, 7, 1, 2000));
 echo date('c', mktime(1, 2, 3, 4, 5, 2006));
 
